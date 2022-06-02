aef8b28e0e2e58f06050e6fcada2c0d0d08ed610
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-02 04:12:18 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
 
-/*
- *main: Serves as an entry point
+/**
+ *
+ *function main: Serves as an entry point
  *
  *Description: Printing a quote using the write function
+ 
  *ssize_t write(int fd, const void *buf, size_t count);
  *
  *Return: This time we return an error 1.
