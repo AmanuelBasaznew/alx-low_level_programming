b1b7a475dfed114ea4c42462ddda3274fbfb59c6
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-02 04:17:30 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,16 @@
 #include <unistd.h>
-
 /**
- *main - Serves as an entry point
+ * main - Entry point
  *
- *Description: Printing a quote using the write function
- 
+ * Description: print a quote using the write function
  *ssize_t write(int fd, const void *buf, size_t count);
- *
- *Return: This time we return an error 1.
+ * 
+ * Return: This time we return an error 1
  */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
 	write(1, quo, 59);
 	return (1);
 }
