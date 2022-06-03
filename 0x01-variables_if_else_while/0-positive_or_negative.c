6c91cf516ce997619d82773faf5fbc8889d7051c
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-03 02:22:39 -0700

    Editing for project 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,26 +2,29 @@
 #include <time.h>
 #include <stdio.h>
 
+
 /**
  * main - Entry point
- *
- * Description: If else statement is used to give different printf results depending on whether the number is positive, negative or zero.
+ * 
+ * Description: print value of n satus; zero, positive or negative
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n;
+		int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX/2; 
-	if (n>0){
-		printf("%d is positive\n", n);
-	}
-		else if (n==0){
-			printf("%d is zero\n", n);
-		}
-	else {printf("%d is negative\n", n);
-	}
-	return (0);
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+
+					/*my code*/
+					if (n > 0)
+								printf("%i is positive\n", n);
+						else if (n == 0)
+									printf("%i is zero\n", n);
+							else
+										printf("%i is negative\n", n);
+
+								return (0);
 }
