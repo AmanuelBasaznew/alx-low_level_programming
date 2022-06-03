f15aaf0ac0839042eff8481741ee2572f3239eaa
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-03 04:36:37 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers with putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit);
+		++digit
+	}
+	putchar('\n');
+	return (0);
+}
