10f829f85c6ac67dca87e09005e9f9fef2211664
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-03 02:15:47 -0700

    Printing alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using the putchar function to print out lower and uppercase alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
