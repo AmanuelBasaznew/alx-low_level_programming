c265792c352a934c53bf0639fd48667466ddaf34
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-03 02:08:22 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using putchar to print all lowercase letters of the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	putchar(alpha);
+	putchar('\n');
+	return (0);
+}
