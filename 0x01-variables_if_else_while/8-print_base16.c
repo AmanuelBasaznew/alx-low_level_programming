151899dea9373b9e329130f428e29b8db38d2e4c
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-03 05:26:06 -0700

    Base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -12,7 +12,7 @@ int main(void)
 {
 	int digit = 48; /*48; decimal rep of 0*/
 
-	while (digit <=102) /*102; decimal rep of f*/
+	while (digit <= 102) /*102; decimal rep of f*/
 	{
 		putchar(digit);
 
