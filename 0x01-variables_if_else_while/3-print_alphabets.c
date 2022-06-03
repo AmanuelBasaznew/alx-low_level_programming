b4ad30855c36f874a0bc11571e94734d9f1d6fe1
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-03 04:11:27 -0700

    Printing alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,23 @@
  * Description: Using while loop for alphabet print
  *
  * Return: Always 0 (Success)
+ *
  */
+
 int main(void)
 {
 	char ch = 'a';
 	
 	char CH = 'A';
 	/*prints a - z*/
-	while (ch <= 'z'){
+	while (ch <= 'z')
+	{
 		putchar(ch);
 		++ch;
 	}
 	/*prints A - Z*/
-	while (CH <= 'Z'){
+	while (CH <= 'Z')
+	{
 		putchar(CH);
 		++CH;
 	}
