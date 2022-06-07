7c89dcd6018d104653eae6eca7c9c086aa77ae00
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-07 03:33:16 -0700

    Alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: Print the alphabet ten times
- *
- * print_alphabet_x10 - prints the alphabet ten times
- *
+ * print_alphabet_x10 - function that prints the alphabet 10 times
  * Return: 0
  */
 
