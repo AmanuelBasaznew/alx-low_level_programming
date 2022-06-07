e19a58153f14e44f6cb50ba855e1d5cab9ecadc3
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-07 03:45:10 -0700

    Uppercase and lowercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,13 +5,13 @@
  *
  * @c: takes input from other functions
  * 
- * Reurn: 1 is c if true else 0
+ * Return: 1 is c if true else 0
  */
 
 int _isalpha(int c)
 {
-	if ( c >= 97 && c <= 122 &&
-			c >= 65 && c <= 90)
+	if (c >= 97 && c <= 122 &&
+			c >= 65 && c >= 90)
 		return (1);
 	return (0);
 }
