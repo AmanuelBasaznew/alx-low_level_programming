49ba31c6e19144c4d0e0e3470b6573ec014fae4a
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-07 03:37:58 -0700

    Lowercase c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,9 @@
 
 /**
  * _islower -  Check for lowercase character c
- * @c - checks input of function
+ *
+ * @c: checks input of function
+ *
  * Return: returns 1 if `c` is lowercase
  *           otherwise returns 0
  */
