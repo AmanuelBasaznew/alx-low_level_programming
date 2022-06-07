b83235301d4a8b275f7236ef430884cfea07f19f
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-07 01:30:39 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Function that prints out the alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+	
+	for (ch, ch <= z, ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
+
