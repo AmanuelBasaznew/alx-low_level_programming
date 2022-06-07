59fb4ea8e6a1f56948a9631fc7acd1a7cf0d9563
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-07 06:41:54 -0700

    Sum until 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 	int i = 0;
-	unsigned long long int a = 0, b = 1, next = 0;
+	unsigned __int64 a = 0, b = 1, next = 0;
 
 	while (i < 98)
 	{
 		next = a + b;
 		a = b;
 		b = next;
-		printf("%lu", next);
+		printf("%llx", next);
 
 		if (i < 97)
 			printf(", ");
