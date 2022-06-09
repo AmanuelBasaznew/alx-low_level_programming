0b21e93c534d3592fcaf6a29484b8375b562d5a1
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-09 03:17:31 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,13 +2,13 @@
 /**
  * more_numbers - prints from 1 - 14 ten times
  *
- * Return: 0
+ * Return: void 
  */
 void more_numbers(void)
 {
 	int i = 0, int j;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		for (j = 0; j <= 14; j++)
 		{
