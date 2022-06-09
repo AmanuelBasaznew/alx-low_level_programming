9a4c67f0899d62ee1e7d142f9fbfc899d0656640
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-09 00:41:43 -0700

    0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints from 1 - 14 ten times
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i, int r;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (r = 0; r < 15; r++)
+		{
+			_putchar(r + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
