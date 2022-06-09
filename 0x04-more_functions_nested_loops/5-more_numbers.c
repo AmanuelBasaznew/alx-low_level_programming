ea021a843ac1a6b48168445222b57c759c1f0efc
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-09 03:24:22 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
  */
 void more_numbers(void)
 {
-	int i = 0, int j;
+	int i, int j;
 
-	while (i < 10)
+	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			_putchar(j + '0');
 		}
-		_putchar('\n');
-		i++
+		_putchar('\r');
 	}
+	_putchar('\n');
 }
