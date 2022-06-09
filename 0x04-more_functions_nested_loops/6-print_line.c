af1ad56ecc724c103c7437de8c56969d7f932179
AmanuelBasaznew <amanuelbasaznew@gmail.com>
2022-06-09 01:02:33 -0700

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - prints a line in the terminal
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		while (n--)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
